ff8980e69359d1fc758a3adc62237666d7faf790
salwamizar <salwasalwamoe2001@gmail.com>
2025-09-23 15:47:17 +0700

    example toast

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,6 +4,11 @@
             <NavigationBar />
         </div>
         <div class="mt-20" for="konten">
+            <Toast title="Kode - Status" message="Perngiriman berhasil" type="success" class="mb-4"/>
+            <Toast title="Kode - Status" message="Perngiriman berhasil" type="warning" class="mb-4"/>
+            <Toast title="Kode - Status" message="Perngiriman berhasil" type="failed" class="mb-4"/>
+            <Toast title="Kode - Status" message="Perngiriman berhasil" type="info" class="mb-4"/>
+
         </div>
     </div>
 </template>
